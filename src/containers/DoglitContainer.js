77bdccc0bc9380bca31cfebb0e86fc6e9d26a6e9
Robin West <robin.west@kobas.co.uk>
2018-04-09 22:32:16 +0100

    Updated React. Logic for grabbing the breed names from the image url.

--- src/containers/DoglitContainer.js
+++ src/containers/DoglitContainer.js
@@ -9,10 +9,39 @@ class DoglitContainer extends React.Component {
 	constructor(){
 		super();
 
-		// this.state = {
-		// 	loading: true,
-		// 	doglitImageUrl: ''
-		// };
+		this.state = {
+			breedName: '',
+			subBreedName: ''
+		};
+	}
+
+	componentDidUpdate(prevProps, prevState){
+		if(this.props.selectedDoglitUrl === prevProps.selectedDoglitUrl)
+			return;
+
+		let names;
+
+		names = this.getNamesFromUrl(this.props.selectedDoglitUrl);
+
+		console.log('names', names);
+
+		this.setState(names);
+	}
+
+	getNamesFromUrl(url){
+		let el   = document.createElement('a'),
+			path = '';
+
+		el.href = url;
+
+		path = el.pathname.replace('/api/img/', '');
+
+		let extraction = path.substring(0, path.indexOf('/')),
+			nameArray  = extraction.split('-');
+
+		console.log('nameArray', nameArray);
+
+		return {breedName: nameArray[0], subBreedName: nameArray[1]};
 	}
 
 	render(){
