36cbb2e3d764d00e578b189f2c3d8fd5607f3cd9
Robin West <robin.west@kobas.co.uk>
2018-04-25 08:54:02 +0100

    Updated the URL parsing function as it seeems dog ceo updated their api routes.

--- src/containers/DoglitContainer.js
+++ src/containers/DoglitContainer.js
@@ -35,7 +35,7 @@ class DoglitContainer extends React.Component {
 		// Create an element and set it's href so we can rip bits out
 		el.href = url;
 
-		path = el.pathname.replace('/api/img/', '');
+		path = el.pathname.replace('/breeds/', '');
 
 		let extraction = path.substring(0, path.indexOf('/')),
 			nameArray  = extraction.split('-');
